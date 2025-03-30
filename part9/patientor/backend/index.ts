0263e9df8ab3e1a67f47c4369514c821b98189db
Ali Ahmed <alitine94@gmail.com>
2025-03-29 23:38:28 -0400

    ...

--- part9/patientor/backend/index.ts
+++ part9/patientor/backend/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import diagnosisRouter from "./routes/diagnosisRouter";
 import patientsRouter from "./routes/patientsRouter";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3000;
 
--- part9/patientor/backend/routes/patientsRouter.ts
+++ part9/patientor/backend/routes/patientsRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import patientsData from "../data/patients";
-import { PatientInfo } from "../types/Patient";
+import { PatientInfo, Patient } from "../types/Patient";
+import { v1 as uuid } from "uuid";
 
 const patientsRouter = Router();
 
@@ -10,6 +11,15 @@ patientsRouter.get("/", (_req, res) => {
   });
 
   res.status(200).json(patientList);
+});
+
+patientsRouter.post("/", (req, res) => {
+  const id = uuid();
+
+  const newPatient = {
+    ...req.body,
+    id,
+  };
 });
 
 export default patientsRouter;
